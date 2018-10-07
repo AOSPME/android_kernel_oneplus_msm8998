fc8b940b40d904827e425a6727e4ef432c14e519
Albere <1OptimizerPro@gmail.com>
2018-10-07 14:55:49 -0400

    fix derp

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -270,7 +270,7 @@ static struct ctl_table sysctl_base_table[] = {
 static int min_sched_granularity_ns __read_only = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns __read_only = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns __read_only;			/* 0 usecs */
-static int max_wakeup_granularity_ns __read_only = NSEC_PER_SEC;	/* 1 second *
+static int max_wakeup_granularity_ns __read_only = NSEC_PER_SEC;	/* 1 second */
 #ifdef CONFIG_SMP
 static int min_sched_tunable_scaling __read_only = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling __read_only = SCHED_TUNABLESCALING_END-1;
